96b3be498306eddec14c06ed227f67fd0e48edfb
alwayswannafly <15007159069@163.com>
2020-08-05 15:50:32 +0800

    基础请求简单封装

---
+++ cnode/src/utils/request.js
@@ -0,0 +1,25 @@
+import Taro, { usePageScroll } from "@tarojs/taro";
+import api from "../constants/api";
+
+export function getJSON(url, data) {
+    return Taro.request({
+        url: url,
+        data: data,
+        method: "GET",
+    });
+}
+export function postJSON(url, data) {
+    return Taro.request({
+        url: url,
+        data: data,
+        method: "POST",
+    });
+}
+
+export async function getTopicList() {
+    let res = await getJSON(api.getTopics).catch((msg) => {
+        console.log("出错了,错误信息:" + msg);
+    });
+    console.log('res返回数据之后,才会执行到这里');
+    return res;
+}
